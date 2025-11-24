327cecd41470f6d5c0311174a808745acda3a007
Chuan-Tung-Cheng <chengreentea0813@gmail.com>
2025-11-24 19:33:29 +0800

    add load recipe to mysql

---
+++ src/albert_icook_crawler/src/pipeline/load/load_recipe.py
@@ -0,0 +1,16 @@
+
+
+def mysql_recipe():
+    """
+    Algorithm:
+    1- connect to MySQL
+    2- create table if not exists
+    3- find csv
+    4- check data type
+    5- load data into the table
+    6- if succeeding, close
+    7- rename file name to mark as processed
+    """
+
+if __name__ == "__main__":
+    mysql_recipe()
